5771bfd28999e9dd0d0248d1ed97f5680e12abea
tadpole <ltadpole@163.com>
2023-07-04 18:47:05 +0800

    添加菜单

---
+++ src/components/menu/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-menu :default-active="defaultActive" :unique-opened="true" class="el-menu-vertical-demo">
+    <template v-for="item in menu">
+      <!-- 如果有子集 -->
+      <template v-if="item.children && item.children.length > 0">
+        <el-sub-menu :key="item.id" :index="item.id" :disabled="item.meta?.disabled">
+          <template #title>
+            <i :class="[item.meta?.icon]"></i>
+            <span slot="title"> {{ item.name }}</span>
+          </template>
+          <MenuTree :menu="item.children" :defaultActive="defaultActive" @clickItem="clickItemHandle" />
+        </el-sub-menu>
+      </template>
+      <!-- 如果没有子集 -->
+      <template v-else>
+        <el-menu-item :key="item.id" :index="item.id" :disabled="item.meta?.disabled" @click="clickItemHandle(item)">
+          <i :class="[item.meta?.icon]"></i>
+          <span slot="title">{{ item.name }}</span>
+        </el-menu-item>
+      </template>
+    </template>
+  </el-menu>
+</template>
+
+<script setup>
+import MenuTree from './index.vue'
+defineProps({
+  menu: {
+    type: Array,
+    required: true,
+    default: () => []
+  },
+  defaultActive: {
+    type: String,
+    required: true,
+    default: []
+  }
+})
+
+const emit = defineEmits(['update-active-path', 'clickItem'])
+
+// 点击当前菜单项
+const clickItemHandle = item => {
+  emit('clickItem', item)
+}
+</script>
--- src/components/sidebar/index.vue
+++ src/components/sidebar/index.vue
@@ -5,46 +5,83 @@
       <h3 class="admin-title">后台管理系统</h3>
     </div>
     <el-menu default-active="2" class="el-menu-vertical" @open="handleOpen" @close="handleClose">
-      <el-sub-menu index="1">
-        <template #title>
-          <el-icon><location /></el-icon>
-          <span>Navigator One</span>
+      <el-menu :default-active="defaultActive" :unique-opened="true" class="el-menu-vertical-demo">
+        <template v-for="item in menu">
+          <template v-if="item.children && item.children.length > 0">
+            <el-sub-menu :key="item.id" :index="item.id" :disabled="item.meta?.disabled">
+              <template #title>
+                <i :class="[item.meta?.icon]"></i>
+                <span slot="title"> {{ item.name }}</span>
+              </template>
+              <MenuTree :menu="item.children" :defaultActive="defaultActive" @clickItem="clickItemHandle" />
+            </el-sub-menu>
+          </template>
+          <template v-else>
+            <el-menu-item
+              :key="item.id"
+              :index="item.id"
+              :disabled="item.meta?.disabled"
+              @click="clickItemHandle(item)"
+            >
+              <i :class="[item.meta?.icon]"></i>
+              <span slot="title">{{ item.name }}</span>
+            </el-menu-item>
+          </template>
         </template>
-        <el-menu-item-group title="Group One">
-          <el-menu-item index="1-1">item one</el-menu-item>
-          <el-menu-item index="1-2">item two</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="Group Two">
-          <el-menu-item index="1-3">item three</el-menu-item>
-        </el-menu-item-group>
-        <el-sub-menu index="1-4">
-          <template #title>item four</template>
-          <el-menu-item index="1-4-1">item one</el-menu-item>
-        </el-sub-menu>
-      </el-sub-menu>
-      <el-menu-item index="2">
-        <el-icon><icon-menu /></el-icon>
-        <span>Navigator Two</span>
-      </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <el-icon><document /></el-icon>
-        <span>Navigator Three</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <el-icon><setting /></el-icon>
-        <span>Navigator Four</span>
-      </el-menu-item>
+      </el-menu>
     </el-menu>
   </div>
 </template>
 
 <script setup>
-// import { Document, Menu as IconMenu, Location, Setting } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+import MenuTree from '../menu/index.vue'
+
+const defaultActive = ref('0')
+
+const menu = ref([
+  {
+    name: '一级',
+    id: '01',
+    level: '1',
+    children: [
+      {
+        id: '010',
+        name: '二级',
+        level: '2',
+        children: [
+          {
+            id: '0110',
+            name: '三级',
+            level: '3'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: '单层',
+    id: '02',
+    level: '1',
+    children: [
+      {
+        name: '二册',
+        id: '020',
+        level: '2'
+      }
+    ]
+  }
+])
+
 const handleOpen = (key, keyPath) => {
   console.log(key, keyPath)
 }
 const handleClose = (key, keyPath) => {
   console.log(key, keyPath)
+}
+
+const clickItemHandle = item => {
+  context.emit('clickItem', item)
 }
 
 const flodClick = () => {}
