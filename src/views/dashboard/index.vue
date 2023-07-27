824ec2f286d51bf672da0afbc0bda56a827be2e6
tadpole <ltadpole@163.com>
2023-07-27 16:49:37 +0800

    首页

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    dashboard
     <div>
       <v-upload
         action="/api/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
@@ -10,13 +9,11 @@
         :drag="true"
       />
     </div>
-    <el-button @click="addRoute">添加路由</el-button>
   </div>
 </template>
 
 <script setup>
 import vUpload from '@/components/upload/index.vue'
-import router from '@/router'
 
 const fileSuccess = file => {
   console.log(file)
@@ -24,15 +21,5 @@ const fileSuccess = file => {
 
 const fileRemove = fileList => {
   console.log('删除了啊', fileList)
-}
-
-const addRoute = async () => {
-  await router.addRoute('Layout', {
-    path: '/uruu',
-    name: 'uruu',
-    components: () => import('@/views/more/list.vue')
-  })
-  console.log(router.getRoutes())
-  router.push('/uruu')
 }
 </script>
