e6a48ff6169dcd2fa24acda6bd39d6966be05344
tadpole <ltadpole@163.com>
2023-08-01 18:26:07 +0800

    eslint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,57 +1,57 @@
 module.exports = {
-    extends: [
-        'stylelint-config-standard',
-        'stylelint-config-recommended-scss',
-        'stylelint-config-standard-vue',
-        'stylelint-config-prettier',
-    ],
-    plugins: ['stylelint-order'],
-    // 不同格式的文件指定自定义语法
-    overrides: [
-        {
-            files: ['**/*.(scss|css|vue|html)'],
-            customSyntax: 'postcss-scss',
-        },
-        {
-            files: ['**/*.(html|vue)'],
-            customSyntax: 'postcss-html',
-        },
-    ],
-    ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts', '**/*.json', '**/*.md', '**/*.yaml', 'dist/**', 'node_modules'],
-    rules: {
-        // 禁止在具有较高优先级的选择器后出现被其覆盖的较低优先级的选择器
-        'no-descending-specificity': null,
-        'selector-pseudo-element-no-unknown': [
-            true,
-            {
-                ignorePseudoElements: ['v-deep'],
-            },
-        ],
-        'selector-pseudo-class-no-unknown': [
-            true,
-            {
-                ignorePseudoClasses: ['deep'],
-            },
-        ],
-        // 禁用每个选择器之前插入空行
-        'rule-empty-line-before': null,
-        // 禁止小于 1 的小数有一个前导零
-        // 'number-leading-zero': 'never',
-        // 一些特殊的scss指令
-        'at-rule-no-unknown': [
-            true,
-            {
-                ignoreAtRules: ['function', 'if', 'else', 'else-if', 'each', 'include', 'mixin'],
-            },
-        ],
-        'at-rule-empty-line-before': [
-            'always',
-            {
-                except: ['blockless-after-same-name-blockless', 'first-nested'],
-                ignore: ['after-comment'],
-                ignoreAtRules: ['else', 'else-if'],
-            },
-        ],
-
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recommended-scss',
+    'stylelint-config-standard-vue',
+    'stylelint-config-prettier',
+  ],
+  plugins: ['stylelint-order'],
+  // 不同格式的文件指定自定义语法
+  overrides: [
+    {
+      files: ['**/*.(scss|css|vue|html)'],
+      customSyntax: 'postcss-scss',
     },
+    {
+      files: ['**/*.(html|vue)'],
+      customSyntax: 'postcss-html',
+    },
+  ],
+  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts', '**/*.json', '**/*.md', '**/*.yaml', 'dist/**', 'node_modules'],
+  rules: {
+    // 禁止在具有较高优先级的选择器后出现被其覆盖的较低优先级的选择器
+    'no-descending-specificity': null,
+    'selector-pseudo-element-no-unknown': [
+      true,
+      {
+        ignorePseudoElements: ['v-deep'],
+      },
+    ],
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['deep'],
+      },
+    ],
+    // 禁用每个选择器之前插入空行
+    'rule-empty-line-before': null,
+    // 禁止小于 1 的小数有一个前导零
+    // 'number-leading-zero': 'never',
+    // 一些特殊的scss指令
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['function', 'if', 'else', 'else-if', 'each', 'include', 'mixin'],
+      },
+    ],
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: ['blockless-after-same-name-blockless', 'first-nested'],
+        ignore: ['after-comment'],
+        ignoreAtRules: ['else', 'else-if'],
+      },
+    ],
+
+  },
 };
